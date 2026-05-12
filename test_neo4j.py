0feb2218842277db398b533cbb593476f3b05cc5
Jinpragya Jain <jpjain132@gmail.com>
2026-05-12 10:23:56 +0530

    updates

--- test_neo4j.py
+++ test_neo4j.py
@@ -3,7 +3,7 @@ from neo4j import GraphDatabase
 # Replace these with your Neo4j Aura details
 NEO4J_URI = "neo4j+s://3bc37e79.databases.neo4j.io"
 NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "WZZC0lOs9zfsMz3xRoHY96BBq69lfCBaFPc38vSzqIc"
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 # Function to test connection
 def test_connection():
